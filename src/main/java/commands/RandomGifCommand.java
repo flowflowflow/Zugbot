71f524749941844a365aebef08555baf48ca18be
Florian Riedlinger <florian.riedlinger@live.de>
2022-11-13 03:19:14 +0100

    add option for random gif response, check for null values in case of empty response from giphy

--- src/main/java/commands/RandomGifCommand.java
+++ src/main/java/commands/RandomGifCommand.java
@@ -4,6 +4,8 @@ import com.kdotj.simplegiphy.SimpleGiphy;
 import com.kdotj.simplegiphy.data.RandomGiphy;
 import com.kdotj.simplegiphy.data.RandomGiphyResponse;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandInteractionOption;
+import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
 import lombok.extern.java.Log;
 import reactor.core.publisher.Mono;
 import util.IOHelper;
@@ -23,19 +25,28 @@ public class RandomGifCommand implements SlashCommand {
     public Mono<Void> handle(ChatInputInteractionEvent event) {
         try {
             IOHelper io = new IOHelper();
-            String giphyApiKey = io.readGiphyApiToken();
 
+            String giphyApiKey = io.readGiphyApiToken();
             SimpleGiphy.setApiKey(giphyApiKey);
             SimpleGiphy giphy = SimpleGiphy.getInstance();
 
-            RandomGiphyResponse randomGiphyResponse = giphy.random("", "r");
-            RandomGiphy randomGiphy = randomGiphyResponse.getRandomGiphy();
+            String searchTerm = event.getOption("keyword")
+                    .flatMap(ApplicationCommandInteractionOption::getValue)
+                    .map(ApplicationCommandInteractionOptionValue::asString)
+                    .orElse("");
 
-            return event.reply(randomGiphy.getUrl());
+            RandomGiphyResponse randomGiphyResponse = giphy.random(searchTerm, "r");
 
+            if (randomGiphyResponse == null) {
+                log.log(Level.WARNING, "Failed deserialization of Giphy response");
+                return event.reply("Sorry, gifs for the keyword couldn't be found :(");
+            } else {
+                RandomGiphy randomGiphy = randomGiphyResponse.getRandomGiphy();
+                return event.reply(randomGiphy.getUrl());
+            }
         } catch (IOException e) {
             log.log(Level.WARNING, "Failed to create IOHelper instance");
-            return event.reply().withEphemeral(true).withContent("Sorry, that didn't work out as planned :(");
+            return event.reply("Sorry, that didn't work out as planned :(");
         }
     }
 }
