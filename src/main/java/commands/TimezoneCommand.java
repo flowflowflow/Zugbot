3c5dcebaff4bd724c6f29090deeae125bef78278
Florian Riedlinger <florian.riedlinger@live.de>
2024-01-18 03:16:22 +0100

    add new command "timezone", not yet active or implemented

---
+++ src/main/java/commands/TimezoneCommand.java
@@ -0,0 +1,21 @@
+package commands;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class TimezoneCommand implements SlashCommand {
+
+    @Override
+    public String getName() {
+        return "timezone";
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+        return null;
+    }
+
+
+}
